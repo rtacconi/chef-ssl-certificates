51fde3c0c551992a34e8c615786d24219a4aa82d
Steffen Gebert <steffen.gebert@typo3.org>
2013-04-08 21:47:19 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 't@binarymarbles.com'
 license          'Apache 2.0'
 description      'Installs and configures SSL certificates.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.1.0'
 
 supports         'debian'
 
